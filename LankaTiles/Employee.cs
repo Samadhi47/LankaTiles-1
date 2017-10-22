74f7170e03a14e785109c815811e58bb1e09efdd
Supun Sashika <supunsashika12345@gmail.com>
2017-10-22 21:39:07 +0530

    Added Employee Class

---
+++ LankaTiles/Employee.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace LankaTiles
+{
+    class Employee
+    {
+        private int empID;
+        private string empName;
+        private string empNIC;
+        private int empContactNo;
+        private string empType;
+        private int dateJoined;
+
+        public void login() { }
+        public void deleteEmp() { }
+        public void addEmp() { }
+        public void updateEmp() { }
+        public void logout() { }
+    }
+}
